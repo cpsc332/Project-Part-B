d0c483a31c85e68bd3dc7816c7c13a4fe1396ea1
KenTree <kennethly909808@csu.fullerton.edu>
2025-12-10 21:10:57 -0800

    Fixed typo, removed string param for csrf_key

--- includes/functions.php
+++ includes/functions.php
@@ -8,8 +8,8 @@ function esc($str) {
 
 
 // Generate a token for security
-function csrf_token($str) {
-    if (!isset($_SESSION['crsf_token'])) {
+function csrf_token() {
+    if (!isset($_SESSION['csrf_token'])) {
         $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
     }
     return $_SESSION['csrf_token'];
@@ -21,7 +21,7 @@ function check_token($token) {
 }
 
 // Allows variables to have a default if result isnt found
-// Ex: 
+// Ex:
 // $id = param('id', 0, 'GET');
 // if failed to GET id, id will be 0, else id will be what was recieved
 function param(string $key, $default = null, string $source = 'REQUEST') {
